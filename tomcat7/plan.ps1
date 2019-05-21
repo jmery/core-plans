3b4f7a1c3e3664396e34f9d0f2cea2d96262277d
Jeff Mery <jeff@merys.net>
2019-05-21 17:35:23 +0000

    Working download and extract

--- tomcat7/plan.ps1
+++ tomcat7/plan.ps1
@@ -1,43 +1,43 @@
-$pkg_name=tomcat7
+$pkg_name="tomcat7"
 $pkg_description="An open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
-$pkg_origin=core
-$pkg_version=7.0.73
+$pkg_origin="jmery"
+$pkg_version="7.0.94"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=('Apache-2.0')
 $pkg_description="The Apache Tomcat software is an open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
 $pkg_upstream_url="http://tomcat.apache.org/"
-$pkg_source=http://archive.apache.org/dist/tomcat/tomcat-7/v${pkg_version}/bin/apache-tomcat-${pkg_version}-windows-x64.zip
-$pkg_shasum=5ec44c0aa56be194cc32f5cf52bab4767a6fef6faa05aab2019233c89f0f3aa0
-$pkg_deps=(core/coreutils)
-$pkg_exports=(
-  [port]=server.port
-)
-$pkg_exposes=(port)
+$pkg_source="http://archive.apache.org/dist/tomcat/tomcat-7/v${pkg_version}/bin/apache-tomcat-${pkg_version}-windows-x64.zip"
+$pkg_shasum="5ec44c0aa56be194cc32f5cf52bab4767a6fef6faa05aab2019233c89f0f3aa0"
+$pkg_deps=@("core/7zip")
+$pkg_exports=@{
+  port="server.port"
+}
+$pkg_exposes=${
+  "port"
+}
 
 # The default implementation extracts your tarball source file into HAB_CACHE_SRC_PATH. The
 # supported archives are: .tar, .tar.bz2, .tar.gz, .tar.xz, .rar, .zip, .Z, .7z. If the file
 # archive could not be found or was not supported, then a message will be printed to stderr
 # with additional information.
 
-do_unpack() {
-  local source_dir=$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}
-  local unpack_file="$HAB_CACHE_SRC_PATH/$pkg_filename"
+#function Invoke-Unpack {
+#  local source_dir=$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}
+#  local unpack_file="$HAB_CACHE_SRC_PATH/$pkg_filename"
 
-  mkdir "$source_dir"
-  pushd "$source_dir" >/dev/null
-  tar xz --strip-components=1 -f "$unpack_file"
+# mkdir "$source_dir"
+#  pushd "$source_dir" >/dev/null
+#  tar xz --strip-components=1 -f "$unpack_file"
 
-  popd > /dev/null
-}
+#  popd > /dev/null
+#}
 
 # The default implementation is to update the prefix path for the configure script to
 # use $pkg_prefix and then run make to compile the downloaded source. This means the
 # script in the default implementation does ./configure --prefix=$pkg_prefix && make. You
 # should override this behavior if you have additional configuration changes to make or
 # other software to build and install as part of building your package.
-do_build() {
-    return 0
-}
+#function Invoke-Build{}
 
 # The default implementation is to run make install on the source files and place the compiled
 # binaries or libraries in HAB_CACHE_SRC_PATH/$pkg_dirname, which resolves to a path like
@@ -45,13 +45,13 @@ do_build() {
 # --prefix option when calling the configure script. You should override this behavior if you
 # need to perform custom installation steps, such as copying files from HAB_CACHE_SRC_PATH
 # to specific directories in your package, or installing pre-built binaries into your package.
-do_install() {
-    build_line "Performing install"
-    mkdir -p "${pkg_prefix}/tc"
-    cp -vR ./* "${pkg_prefix}/tc"
+#function Invoke-Install{
+#    build_line "Performing install"
+#    mkdir -p "${pkg_prefix}/tc"
+#    cp -vR ./* "${pkg_prefix}/tc"
 
     # default permissions included in the tarball don't give any world access
-    find "${pkg_prefix}/tc" -type d -exec chmod -v 755 {} +
-    find "${pkg_prefix}/tc" -type f -exec chmod -v 644 {} +
-    find "${pkg_prefix}/tc" -type f -name '*.sh' -exec chmod -v 755 {} +
-}
+#    find "${pkg_prefix}/tc" -type d -exec chmod -v 755 {} +
+#    find "${pkg_prefix}/tc" -type f -exec chmod -v 644 {} +
+#    find "${pkg_prefix}/tc" -type f -name '*.sh' -exec chmod -v 755 {} +
+#}
